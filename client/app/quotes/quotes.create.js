669307b824c4417e20b532b18e45eeab353332b8
ben-cunningham <bencunningham17@gmail.com>
2016-05-03 20:47:20 -0700

    Fix syntax bug

--- client/app/quotes/quotes.create.js
+++ client/app/quotes/quotes.create.js
@@ -1,43 +1,58 @@
 'use strict';
 
-angular.module('myApp.quotes').controller('NewQuoteCtrl', ['$scope', '$stateParams','Quotes', 'Houses', 'Estimators',
-    function($scope, $stateParams, Quotes, Houses, Estimators) {
+angular.module('myApp.quotes').controller('NewQuoteCtrl', ['$scope', '$stateParams','Quotes', 'Houses', 'Estimators', '$window',
+    function($scope, $stateParams, Quotes, Houses, Estimators, $window) {
 
         $scope.showNewHouse = true;
 
         $scope.toggleNewHouse = function() {
-          if($scope.showNewHouse){
-             angular.element('.new-house').show();
-             angular.element('.existing-house').hide();
-             $scope.showNewHouse = true;
-          }
-          else {
-             angular.element('.new-house').hide();
-             angular.element('.existing-house').show();
-             $scope.showNewHouse = false;
-         }
+            if($scope.showNewHouse){
+                angular.element('.new-house').show();
+                angular.element('.existing-house').hide();
+                $scope.showNewHouse = true;
+            }
+            else {
+                angular.element('.new-house').hide();
+                angular.element('.existing-house').show();
+                $scope.showNewHouse = false;
+            }
         };
 
         $scope.toggleNewHouse();
 
         $scope.quote = null;
-        $scope.houses = Houses.getHouses(function() {
-
-        });
+        $scope.houses = Houses.getHouses(function() {});
 
         $scope.createQuote = function() {
             // User wants to add a new house
             // instead of selecting an existing one
+            console.log("test");
             if($scope.showNewHouse){
                 Houses.addHouse($scope.newHouse, function(id) {
                     $scope.quote.house = id;
-                    Quotes.addQuote($scope.quote);
+                    addQuote($scope.quote);
                 });
             }
             else {
                 $scope.quote.house = $scope.house;
-                Quotes.addQuote($scope.quote);
+                addQuote($scope.quote);
             }
+        };
+
+        function addQuote(quote) {
+            Quotes.addQuote(quote,
+                function() {
+                    $window.location.href = '/quotes'
+                    console.log("controller");
+                },
+                function(error) {
+                    handleQuoteError(error);
+                }
+            );
+        };
+
+        function handleError(error) {
+            alert("Encountered error: " +error);
         };
     }
 ]);
