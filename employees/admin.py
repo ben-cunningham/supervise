0c2e13f48fd964d40630489420ef74f5829ea2ec
ben-cunningham <bencunningham17@gmail.com>
2016-05-18 12:41:15 -0700

    Register tasks and employees for admin site

---
+++ employees/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from models import Estimator, Foreman
+
+admin.site.register(Estimator)
+admin.site.register(Foreman)
---
+++ tasks/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import Job, Quote, CheckIn
+
+admin.site.register(Job)
+admin.site.register(Quote)
+admin.site.register(CheckIn)
