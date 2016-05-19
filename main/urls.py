19181fc67e36d039611153ed852292421f583d6b
ben-cunningham <bencunningham17@gmail.com>
2016-05-19 09:22:56 -0700

    Silence django warning about passing in views as strings

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = (
     url(r'^houses/$', views.HouseList.as_view()),
     url(r'^houses/(?P<pk>[0-9]+)/$', views.HouseDetail.as_view()),
-    url(r'^login/$', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'^verify-token/$', 'rest_framework_jwt.views.refresh_jwt_token'),
+    url(r'^login/$', obtain_jwt_token),
+    url(r'^verify-token/$', refresh_jwt_token),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
