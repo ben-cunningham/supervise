74cfbc660ce99b1f096cd235bf7844a5be6f1281
ben-cunningham <bencunningham17@gmail.com>
2016-05-24 13:57:41 -0700

    Remove console.log statement

--- client/app/jobs/jobs.detail.js
+++ client/app/jobs/jobs.detail.js
@@ -10,7 +10,6 @@ angular.module('myApp.jobs')
   .controller('JobDetailCtrl', ['$scope', '$stateParams', 'Jobs', 'Estimator', '$state',
       function($scope, $stateParams, Jobs, Estimator, $state) {
           $scope.job = Jobs.getJob({ pk: $stateParams.pk }, function() {
-              console.log($scope.job);
               $scope.data = [$scope.job.current_hours_spent, $scope.job.budget];
               $scope.job_type = job_type[$scope.job.job_type];
           });
