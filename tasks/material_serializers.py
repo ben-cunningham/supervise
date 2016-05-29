aa561aa6c994a86d5644eba29c5222b20de29769
ben-cunningham <bencunningham17@gmail.com>
2016-05-28 17:52:45 -0700

    Fix create() bug

--- tasks/material_serializers.py
+++ tasks/material_serializers.py
@@ -10,31 +10,7 @@ from team.models import (
     Team,
 )
 
-class MaterialSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Material
-        fields = (
-            'pk',
-            'total_quantity',
-            'units',
-            'team',
-            'checked_out_materials',
-        )
-
-    def create(self, validated_data):
-        view = self.context['view']
-        team_pk = view.kwargs['team_pk']
-        team = Team.objects.get(pk=team_pk)
-        material = Material.objects.create(
-            name=validated_data['name'],
-            quantity=validated_data['quantity'],
-            units=validated_data['units'],
-            team=team,
-        )
-
-        return material
-
+from units import UNIT_CHOICES
 
 
 class CheckedOutMaterialSerializer(serializers.ModelSerializer):
@@ -60,3 +36,33 @@ class CheckedOutMaterialSerializer(serializers.ModelSerializer):
         )
 
         return checked_out_material
+
+class MaterialSerializer(serializers.ModelSerializer):
+    units = serializers.ChoiceField(choices=UNIT_CHOICES)
+    team = serializers.PrimaryKeyRelatedField(required=False, read_only=True)
+    checked_out_materials = CheckedOutMaterialSerializer(many=True, required=False)
+
+    class Meta:
+        model = Material
+        fields = (
+            'pk',
+            'name',
+            'total_quantity',
+            'units',
+            'team',
+            'checked_out_materials',
+        )
+
+    def create(self, validated_data):
+        view = self.context['view']
+        team_pk = view.kwargs['team_pk']
+        team = Team.objects.get(pk=team_pk)
+        print validated_data
+        material = Material.objects.create(
+            name=validated_data['name'],
+            total_quantity=validated_data['total_quantity'],
+            units=validated_data['units'],
+            team=team,
+        )
+
+        return material
