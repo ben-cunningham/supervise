935a88cc898e752db16732b0c3858df8216dd086
ben-cunningham <bencunningham17@gmail.com>
2016-06-16 21:08:46 -0700

    Silence pyCharm warnings

---
+++ main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from models import Job, Quote, CheckIn, Material
+
+admin.site.register(Job)
+admin.site.register(Quote)
+admin.site.register(CheckIn)
+admin.site.register(Material)
--- main/views.py
+++ main/views.py
@@ -2,10 +2,12 @@ from models import House
 from serializers import HouseSerializer
 from rest_framework import generics
 
+
 class HouseList(generics.ListCreateAPIView):
-	queryset = House.objects.all()
-	serializer_class = HouseSerializer
+    queryset = House.objects.all()
+    serializer_class = HouseSerializer
+
 
 class HouseDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = House.objects.all()
-	serializer_class = HouseSerializer
+    queryset = House.objects.all()
+    serializer_class = HouseSerializer
