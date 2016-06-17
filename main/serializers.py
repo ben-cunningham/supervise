904ca68dd54d377f4ce9d2757f1bfa2fef523c7e
ben-cunningham <bencunningham17@gmail.com>
2016-06-17 12:22:03 -0700

    Fix house creation

--- main/serializers.py
+++ main/serializers.py
@@ -8,6 +8,8 @@ class AddressSerializer(serializers.ModelSerializer):
     Serializer for the Address model
     """
 
+    line2 = serializers.CharField(required=False)
+
     class Meta:
         model = Address
         fields = (
@@ -36,24 +38,32 @@ class HouseSerializer(serializers.ModelSerializer):
         view = self.context['view']
         team_pk = view.kwargs['team_pk']
         team = Team.objects.get(pk=team_pk)
+
         try:
             address = Address.objects.create(
-                line1=validated_data['line1'],
-                line2=validated_data['line2'],
-                city=validated_data['city'],
-                state=validated_data['state'],
-                zip=validated_data['zip'],
-                country=validated_data['country'],
+                line1=validated_data['address']['line1'],
+                city=validated_data['address']['city'],
+                state=validated_data['address']['state'],
+                zip=validated_data['address']['zip'],
+                country=validated_data['address']['country'],
             )
+
+            if 'line2' in validated_data['address']:
+                address.zip = validated_data['address']['line2']
+
             address.save()
+
+            print address
+            print team
 
             house = House.objects.create(
                 address=address,
-                team=team,
+                company=team,
             )
 
             house.save()
             return house
-        except:
+        except Exception, e:
+            print e
             return None
 
