55bd9b2e6e0e9f93a1197d7d24647ed5a0ad044f
ben-cunningham <bencunningham17@gmail.com>
2016-06-17 12:21:43 -0700

    Add team_pk to house urls

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 urlpatterns = (
-    url(r'^houses/$', views.HouseList.as_view()),
-    url(r'^houses/(?P<pk>[0-9]+)/$', views.HouseDetail.as_view()),
+    url(r'^teams/(?P<team_pk>[0-9]+)/houses/$', views.HouseList.as_view()),
+    url(r'^teams/(?P<team_pk>[0-9]+)/houses/(?P<pk>[0-9]+)/$', views.HouseDetail.as_view()),
     url(r'^login/$', obtain_jwt_token),
     url(r'^verify-token/$', verify_jwt_token),
 )
