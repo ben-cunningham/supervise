0c1c7bc61a614a46da9c58000d9a0dbe95ed9f1a
ben-cunningham <bencunningham17@gmail.com>
2016-06-21 15:56:21 -0700

    Start adding /me view

--- employees/urls.py
+++ employees/urls.py
@@ -7,6 +7,7 @@ urlpatterns = (
     url(r'^estimators/$', views.EstimatorList.as_view()),
     url(r'^foreman/$', views.ForemanList.as_view()),
     url(r'^foreman/(?P<pk>[0-9]+)/$', views.ForemanDetail.as_view()),
+    url(r'^me$', views.Me.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- employees/views.py
+++ employees/views.py
@@ -1,18 +1,23 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 from serializers import EstimatorSerializer, ForemanSerializer
 from models import Estimator, Foreman, Employee
 from team.models import Team
+
 
 class EstimatorList(generics.ListCreateAPIView):
     queryset = Estimator.objects.all()
     serializer_class = EstimatorSerializer
 
+
 class EstimatorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Estimator.objects.all()
     serializer_class = EstimatorSerializer
 
-class ForemanList(generics.ListCreateAPIView):
 
+class ForemanList(generics.ListCreateAPIView):
     def get_queryset(self):
         employee = Employee.objects.get(user=self.request.user)
         team = Team.objects.get(pk=employee.team.pk)
@@ -24,6 +29,20 @@ class ForemanList(generics.ListCreateAPIView):
 
     serializer_class = ForemanSerializer
 
+
 class ForemanDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Foreman.objects.all()
     serializer_class = ForemanSerializer
+
+
+class Me(APIView):
+    """
+    View to get the current user's profile, as
+    well as update their settings
+    """
+
+    def get(self, request, format=None):
+        pass
+
+    def post(self, request, format=None):
+        pass
