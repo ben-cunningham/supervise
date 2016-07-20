d270c423056edf4086e73b600e483ded5aa63df8
ben-cunningham <bencunningham17@gmail.com>
2016-07-19 21:43:58 -0700

    Add spinner styling

--- client/app/shared/spinner.js
+++ client/app/shared/spinner.js
@@ -5,10 +5,33 @@
         .module('myApp.shared')
         .directive('loadingSpinner', loadingSpinner);
 
-    function loadingSpinner() {
+    loadingSpinner.$inject = ['$http', '$rootScope'];
+
+    function loadingSpinner($http, $rootScope) {
         return {
             restrict: 'A',
-            template: '<div class="loading-spinner"></div>',
+            template: '<div class="loading-overlay"><div class="loading-spinner"></div></div>',
+            link: function (scope, elm, attrs) {
+
+                if (attrs.$attr.usSpinnerStandalone) return;
+
+                $rootScope.spinnerActive = false;
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (loading) {
+
+                    $rootScope.spinnerActive = loading;
+                    if (loading) {
+
+                        elm.removeClass('ng-hide');
+                    } else {
+
+                        elm.addClass('ng-hide');
+                    }
+                });
+            }
         };
     }
 
