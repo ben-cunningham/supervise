c83b66e7f329433127bef84be935e38ca479b320
ben-cunningham <bencunningham17@gmail.com>
2016-07-19 22:09:30 -0700

    Conditionally show 'completed jobs'

--- client/app/jobs/jobs.list.js
+++ client/app/jobs/jobs.list.js
@@ -2,13 +2,23 @@
     'use strict';
 
     angular.module('myApp.jobs').controller('JobListCtrl', JobListController);
-    
-    JobListController.$inject = ['$scope', 'Jobs'];
 
-    function JobListController($scope, Jobs) {
+    JobListController.$inject = ['$scope', '$filter', 'Jobs'];
 
-        $scope.jobs = Jobs.getJobs(function() {
+    function JobListController($scope, $filter, Jobs) {
 
+        Jobs.getJobs(function(jobs) {
+
+            $scope.jobs = $filter('filter')(jobs, { completed : false });
+            $scope.completedJobs = $filter('filter')(jobs, { completed : true });
+
+            if($scope.completedJobs.length == 0) {
+
+                $scope.hasCompleted = false;
+            } else {
+                
+                $scope.hasCompleted = true;
+            }
         });
     }
 
