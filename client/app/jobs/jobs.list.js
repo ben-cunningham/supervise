649495dd28c99394d2623bd47b32aaf974679407
ben-cunningham <bencunningham17@gmail.com>
2016-07-19 21:53:49 -0700

    Change JobListController to be immediatly invoked

--- client/app/jobs/jobs.list.js
+++ client/app/jobs/jobs.list.js
@@ -1,7 +1,17 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('myApp.jobs').controller('JobListCtrl', function($scope, Jobs) {
-    $scope.jobs = Jobs.getJobs(function() {
-        
-    });
-});
+    angular.module('myApp.jobs').controller('JobListCtrl', JobListController);
+    
+    JobListController.$inject = ['$scope', 'Jobs'];
+
+    function JobListController($scope, Jobs) {
+
+        $scope.jobs = Jobs.getJobs(function() {
+
+        });
+    }
+
+})();
+
+
