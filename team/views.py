655ae272c4e5e4f96178523364ec53fbeb619e73
ben-cunningham <bencunningham17@gmail.com>
2016-08-21 17:45:47 -0700

    Override default APIView methods

--- team/views.py
+++ team/views.py
@@ -1,20 +1,42 @@
 from rest_framework import generics
 from serializers import TeamSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view , permission_classes
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from invitation.models import Invitation
 from employees.models import Employee
+from employees.permissions import IsAdminPermission
 from models import Team
 
 class TeamList(generics.ListCreateAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
-class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Team.objects.all()
-    serializer_class = TeamSerializer
+
+class TeamDetail(APIView):
+    """
+    """
+
+    def get(self, request, pk):
+        team = Team.objects.get(pk=pk)
+        team_serializer = TeamSerializer(team)
+        return Response(team_serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        employee = Employee.objects.get(user=request.user)
+        if not employee or not employee.is_admin:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        team = Team.objects.get(pk=pk)
+        team_serializer = TeamSerializer(team, data=request.data, partial=True)
+        if team_serializer.is_valid():
+            team_serializer.save()
+            return Response(team_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def create_invitation(request, team_pk):
